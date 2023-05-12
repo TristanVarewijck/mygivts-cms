c8a0fdbb6c7b1c7492a217cb89ee378b0fc74315
TristanVarewijck <tristanconnect4@outlook.com>
2023-05-12 15:07:22 +0200

    fix: add product listing modal

---
+++ src/api/product-listing/routes/product-listing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-listing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-listing.product-listing');
