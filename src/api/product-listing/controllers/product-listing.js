d4fc978d26f3caa733ee1bab2da9ee76c45449ec
TristanVarewijck <tristanconnect4@outlook.com>
2023-09-09 13:59:48 +0200

    fix: 2 new fields

--- src/api/product-listing/controllers/product-listing.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * product-listing controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::product-listing.product-listing');
--- src/api/product-listing/services/product-listing.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * product-listing service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::product-listing.product-listing');
